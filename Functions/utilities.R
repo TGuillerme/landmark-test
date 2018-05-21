493d09d32a6a1f37a6e78a1079f4173f28e7309c
TGuillerme <guillert@tcd.ie>
2018-05-22 09:36:58 +1000

    Renamed analysis files

--- Functions/utilities.R
+++ Functions/utilities.R
@@ -327,3 +327,276 @@ reduce.check<-function(AllData, AllClassifiers){
     names(check_output)<-names(AllData)
     return(list(coords_PLS_output, check_output))
   }
+
+
+
+#@param CI: the confidence interval level
+#@param rarefaction: whether to use the rarefied results (TRUE) or not (FALSE)
+#@param print.token: whether to add the significance tokens (i.e. stars)
+#@param rounding: the number of digits to print after 0.
+#@param path: the path to the results
+#@param species: the list of species as written in the results
+#@param datasets: the names of the datasets as written in the results
+#@param partitions: optional, the name of the landmark partitions
+summarise.results <- function(CI, rarefaction, print.token = FALSE, rounding = 3, path = "../Data/Results/", species = c("Wombat", "Wombat_krefftii", "Wombat_latifrons", "Wombat_ursinus", "Wombat_lasiorhinus"), datasets = c("cranium", "mandible"), partitions = c("zygom", "restC", "snout", "back", "front", "restM")) {
+
+    ## Printing significance tokens
+    get.token <- function(p) {
+        if(p > 0.05) {
+            return("")
+        }
+        if(p < 0.05 && p > 0.01) {
+            return(".")
+        }
+        if(p < 0.01 && p > 0.001) {
+            return("*")
+        }
+        if(p < 0.001) {
+            return("**")
+        }
+    }
+
+    ## Make the empty table
+    results_table <- data.frame(matrix(NA, ncol = length(species)*4, nrow = 6+1))
+
+    ## Loop through the datasets
+    for(sp in 1:length(species)) {
+
+        for(ds in 1:length(datasets)) {
+
+            ## Extract the results
+            if(rarefaction) {
+                load(paste0(path, species[sp], "_", datasets[ds], "rarefied_CI", CI, ".Rda"))
+            } else {
+                load(paste0(path, species[sp], "_", datasets[ds], "_CI", CI, ".Rda"))
+            }
+
+            ## Summarise the results
+            difference <- make.table(results$difference, correction = "bonferroni")
+            overlap <- make.table(results$overlaps, correction = "bonferroni")
+
+            ## Fill the table
+            if(ds == 1) {
+                ## Values
+                results_table[2:4, 1+(4*(sp-1))] <- round(difference[,2], digits = rounding)
+                results_table[2:4, 3+(4*(sp-1))] <- round(overlap[,2], digits = rounding-1)
+                ## Signif
+                if(print.token) {
+                    results_table[2:4, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding),  sapply(difference[,6], get.token))
+                    results_table[2:4, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding),  sapply(overlap[,6], get.token))
+                } else {
+                    results_table[2:4, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding))
+                    results_table[2:4, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding))
+                }
+
+            } else {
+                ## Values
+                results_table[5:7, 1+(4*(sp-1))] <- round(difference[,2], digits = rounding)
+                results_table[5:7, 3+(4*(sp-1))] <- round(overlap[,2], digits = rounding-1)
+                ## Signif
+                if(print.token) {
+                    results_table[5:7, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding),  sapply(difference[,6], get.token))
+                    results_table[5:7, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding),  sapply(overlap[,6], get.token))
+                } else {
+                    results_table[5:7, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding))
+                    results_table[5:7, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding))
+                }
+            }
+        }
+    }
+
+    ## Renaming the table elements
+    if(!missing(partitions)) {
+        rownames(results_table) <- c("test", partitions)
+    } else {
+        rownames(results_table) <- c("test", paste("Cranium", c(1,2,3)), paste("Mandible", c(1,2,3)))
+    }
+    results_table[1,] <- rep(c("diff", "p", "overlap", "p"), length(species))
+    colnames(results_table) <- names(unlist(sapply(species, rep, 4, simplify = FALSE)))
+
+    ## Flip the table
+    return(t(results_table))
+
+}
+
+
+
+# plot.test.results <- function(data, rarefaction, cols = c("grey", "green", "magenta"), ...) {
+
+#     ## Returning significance
+#     get.token <- function(p) {
+#         if(p > 0.05) {
+#             return(FALSE)
+#         }
+#         if(p < 0.05 && p > 0.01) {
+#             return(FALSE)
+#         }
+#         if(p < 0.01 && p > 0.001) {
+#             return(TRUE)
+#         }
+#         if(p < 0.001) {
+#             return(TRUE)
+#         }
+#     }
+
+# CI <- 95
+
+# species <- c("Wombat", "Wombat_krefftii", "Wombat_latifrons", "Wombat_ursinus", "Wombat_lasiorhinus")
+# datasets <- c("cranium", "mandible")
+
+# rounding = 3
+
+# results_table <- data.frame(matrix(NA, ncol = length(species)*4, nrow = 6+1))
+
+# for(sp in 1:length(species)) {
+
+#     for(ds in 1:length(datasets)) {
+
+#         ## Extract the results
+#         load(paste0("../Data/Results/", species[sp], "_", datasets[ds], "_CI", CI, ".Rda"))
+
+#         ## Summarise the results
+#         difference <- make.table(results$difference, correction = "bonferroni")
+#         overlap <- make.table(results$overlaps, correction = "bonferroni")
+
+#         ## Fill the table
+#         if(ds == 1) {
+#             ## Values
+#             results_table[2:4, 1+(4*(sp-1))] <- round(difference[,2], digits = rounding)
+#             results_table[2:4, 3+(4*(sp-1))] <- round(overlap[,2], digits = rounding-1)
+#             ## Signif
+#             results_table[2:4, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding),  sapply(difference[,6], get.token)) # Remove the first component of the paste if only tokens needed
+#             results_table[2:4, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding),  sapply(overlap[,6], get.token))
+
+#         } else {
+#             ## Values
+#             results_table[5:7, 1+(4*(sp-1))] <- round(difference[,2], digits = rounding)
+#             results_table[5:7, 3+(4*(sp-1))] <- round(overlap[,2], digits = rounding-1)
+#             ## Signif
+#             results_table[5:7, 2+(4*(sp-1))] <- paste0(round(difference[,6], digits = rounding),  sapply(difference[,6], get.token))
+#             results_table[5:7, 4+(4*(sp-1))] <- paste0(round(overlap[,6], digits = rounding),  sapply(overlap[,6], get.token))
+#         }
+#     }
+# }
+
+# ## Renaming the table elements
+# rownames(results_table) <- c("test", paste("Cranium", c(1,2,3)), paste("Mandible", c(1,2,3)))
+# results_table[1,] <- rep(c("diff", "p", "overlap", "p"), length(species))
+# colnames(results_table) <- names(unlist(sapply(species, rep, 4, simplify = FALSE)))
+
+# ## Flip the table
+# results_table <- t(results_table)
+
+
+# ##
+# kable(results_table, digits = 4)
+
+
+
+
+#     ## Formatting data data as a matrix for image
+#     age_rows <- list(c(1:7), c(8:14), c(15:21))
+#     epoch_rows <- list(c(22:28), c(29:35), c(36:42))
+#     signif_columns <- c(10:12)
+
+#     ## Combining the age matrix
+#     if(type == "age") {
+#         data_matrix <- as.matrix(do.call(cbind, lapply(age_rows, function(rows, cols, data) return(data[rows, cols]), cols = signif_columns, data = data)))
+#     } else {
+#         data_matrix <- as.matrix(do.call(cbind, lapply(epoch_rows, function(rows, cols, data) return(data[rows, cols]), cols = signif_columns, data = data)))
+#     }
+
+#     ## Convert into binary
+#     data_matrix <- ifelse(data_matrix, 1, 0)
+#     data_matrix <- ifelse(is.na(data_matrix), 0.5, data_matrix)
+
+#     ## Colours equivalent
+#     request_colors <- length(unique(as.vector(data_matrix)))
+#     if(request_colors == 3) {
+#         ## Reorder colours (damn image()!)
+#         cols <- cols[c(2,3,1)]
+#     }
+
+#     colours_table <- matrix(c(1,0,0.5), ncol = 1, dimnames = list(cols))
+#     num_cols <- na.omit(sort(match(unique(as.vector(data_matrix)), colours_table)))
+
+#     ## Selecting the colours
+#     colours <- rownames(colours_table)[num_cols]
+
+#     ## Plot the matrix
+#     image(t(data_matrix[7:1,]), col = rev(colours), xaxt = "n", yaxt = "n", main = main, ...)
+#     # image(t(data_matrix[7:1,]), col = rev(cols), xaxt = "n", yaxt = "n", main = main)
+#     ## Add the lines
+#     abline(v = c(0.3125, 0.6875), lty = 2)
+#     ## Add the y axis
+#     if(yaxis) axis(2, at = seq(from = 0, to = 1, by = 1/6), las = 2, label = rev(data$model[age_rows[[1]]]), tick = FALSE)
+#     ## Add the x axis
+#     if(xaxis) axis(1, at = c(0.125, 0.5, 0.85), label = c("stratigraphy", "duration", "number"), tick = FALSE)
+#     ## Add the upper x axis
+#     if(xaxis2) axis(3, at = c(seq(from = 0, to = 1, by = 1/8)), label = rep(c("e:1", "e:2", "e:3"), 3), tick = FALSE, padj = 1.5, cex = 0.8)
+#     ## Add the right y axis
+#     if(yaxis2) axis(4, at = 0.5, tick = FALSE, labels = type)
+# }
+
+
+# multi.plot.extinction <- function(data, type, cols = c("blue", "orange", "white"), main = "", data.names, ...) {
+
+#     ## Check how many types
+#     if(length(type) == 1) {
+
+#         ## Setting up plot layout
+#         plot_layout <- layout(matrix(c(1:length(data)), 1, length(data), byrow = TRUE), rep(1, length(data)), rep(1, length(data)), FALSE)
+#         #layout.show(plot_layout)
+
+#         ## First plot
+#         par(mar = c(4, 6, 4, 0)) #c(bottom, left, top, right)
+#         plot.extinction(data[[1]], type = type, col = cols, xaxis = TRUE, yaxis = TRUE, xaxis2 = TRUE, main = data.names[[1]], ...)
+
+#         ## Other plots
+#         for(slug in 2:length(data)) {
+#             par(mar = c(4, 0, 4, 0)) #c(bottom, left, top, right)
+#             plot.extinction(data[[slug]], type = type, col = cols, main = data.names[[slug]], xaxis = TRUE, xaxis2 = TRUE, ...)
+#         }
+#     } else {
+
+#         ## Setting up plot layout
+#         plot_layout <- layout(matrix(c(1:(length(data)*2)), 2, length(data), byrow = TRUE), rep(1, length(data)*2), rep(1, length(data)*2), FALSE)
+#         #layout.show(plot_layout)
+
+#         ## First plot (first row)
+#         par(mar = c(0, 6, 4, 0)) #c(bottom, left, top, right)
+#         plot.extinction(data[[1]], type = type[1], col = cols, yaxis = TRUE, xaxis2 = TRUE, main = data.names[[1]], ...)
+
+#         # ## Other plots (first row)
+#         # for(slug in 2:(length(data)-1)) {
+#         #     par(mar = c(0, 0, 4, 0)) #c(bottom, left, top, right)
+#         #     plot.extinction(data[[slug]], type = type[1], col = cols, main = data.names[[slug]], xaxis2 = TRUE, ...)
+#         # }
+
+#         ## Last plot (first row)
+#         par(mar = c(0, 0, 4, 3)) #c(bottom, left, top, right)
+#         plot.extinction(data[[length(data)]], type = type[1], col = cols, main = data.names[[length(data)]], xaxis2 = TRUE, yaxis2 = TRUE, ...)
+
+#         ## First plot (second row)
+#         par(mar = c(4, 6, 0, 0)) #c(bottom, left, top, right)
+#         plot.extinction(data[[1]], type = type[2], col = cols, yaxis = TRUE, xaxis = TRUE, ...)
+
+#         # ## Other plots (second row)
+#         # for(slug in 2:(length(data)-1)) {
+#         #     par(mar = c(4, 0, 0, 0)) #c(bottom, left, top, right)
+#         #     plot.extinction(data[[slug]], type = type[2], col = cols, xaxis = TRUE, ...)
+#         # }
+
+#         ## Last plot (first row)
+#         par(mar = c(4, 0, 0, 3)) #c(bottom, left, top, right)
+#         plot.extinction(data[[length(data)]], type = type[2], col = cols, xaxis = TRUE, yaxis2 = TRUE, ...)
+#     }
+# }
+
+
+
+
+
+
+
+
