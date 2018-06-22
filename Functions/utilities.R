835b438ff196b28063c2f39422c3413d6996959a
Vera Weisbecker <uqvweisb@uq.edu.au>
2018-06-22 15:41:04 +1000

    Revert "made plotreftotarget heat code for PC capable of finding PC axes"
    This reverts commit 04c48c5eec0c3101dfe0f596e10b22a3b2c5f19f.

--- Functions/utilities.R
+++ Functions/utilities.R
@@ -325,7 +325,6 @@ PCA.vectors<-function(x, minfirst=TRUE){
   }
 }
 
-
 #procD code (for procD.allometry and procD.lm) analysis
 
 #@param formula: a formula object (e.g. coords ~ Csize)
@@ -339,7 +338,6 @@ handle.procD.formula <- function(formula, procrustes, procD.fun = procD.allometr
   return(procD.fun(f1 = formula, data = geomorph_data_frame, ...))
 }
 
-heatplot.PCs(CW$cranium, minfirst=FALSE, PC_axis=1)
 
 #Allometry analysis (based on above handle.procD.formula)
 
